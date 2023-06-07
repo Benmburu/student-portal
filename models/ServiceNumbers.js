c29358d7ccf622673aa70a32c9ebc2ff777ce94f
BenMburu <bennjuguna0@gmail.com>
2023-06-07 22:24:07 +0000

    connecting to mongoDB

--- models/ServiceNumbers.js
+++ models/ServiceNumbers.js
@@ -1,12 +1,7 @@
 import mongoose from "mongoose";
 
 const serviceNumberSchema = new mongoose.Schema(
-    {
-        serviceNumber: {
-            _id: new mongoose.Types.ObjectId(),
-            type: String,
-        },
-    }
+    {}
 )
 
-export default mongoose.model("reg_number", serviceNumberSchema)
+export default mongoose.model("serviceNumber", serviceNumberSchema, "reg_number")
